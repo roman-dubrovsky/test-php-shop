a531aa22a8fa60b5da0b5b1668841867361c14c9
Roman Dubrovsky <roma2107946@gmail.com>
2016-04-04 12:28:16 +0300

    Change configs

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-  $shop_id = 31;
-  $product_id = 31;
-  $token = "0560e7021c606ac793610adc0e956078";
+  $shop_id = 34;
+  $product_id = 34;
+  $token = "ef0aeb6f2bfba13b5fcd929b71749c10";
   $api_url = 'http://upn-staging.ru/api/shops/v2/shops/';
 ?>
